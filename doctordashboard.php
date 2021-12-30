4c89a9e1a2076905dcbceb8db5c3759f6c4cbc08
gnishaanthini <nishaanthini.19@cse.mrt.ac.lk>
2021-12-30 21:10:58 +0530

    comit changes

--- doctordashboard.php
+++ doctordashboard.php
@@ -1,21 +1,7 @@
 <?php
 
-session_start();
+require_once 'doctor_dash.php';
 
-
-require_once  'classes/doctor.php';
-
-$uname = $_SESSION['username'];
-
-if($_SESSION['qualified'] == false){
-    $status = "You can't have an account here";
-}
-else{
-    $status = $uname;
-}
-
-$doctor = Doctor::getInstance($uname);
-$patients = $doctor->getPatientList($uname);
 ?>
 
 
